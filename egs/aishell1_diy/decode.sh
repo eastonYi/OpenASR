541d9315916f246fcba7505ab7949cbed8e6ca28
eastonYi <yicheng1994@outlook.com>
2020-07-31 18:51:16 +0800

    fix

--- egs/aishell1_diy/decode.sh
+++ egs/aishell1_diy/decode.sh
@@ -4,15 +4,13 @@ source path.sh
 expdir=$1
 model_type=$2
 gpu=$3
-
-# ep=avg-last1
-ep=${4:?"avg-last10"}
+ep=$4
 decode_dir=$expdir/decode_test_${ep}
 mkdir -p $decode_dir
 
 CUDA_VISIBLE_DEVICES=$gpu \
 python -W ignore::UserWarning $SRC_ROOT/decode.py \
-    --feed-batchsize 80 \
+    --batch_frames 10000 \
     --nbest 5 \
     --use_gpu True \
     $model_type \
