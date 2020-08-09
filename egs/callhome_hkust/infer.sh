1160b63f85a65d2febe148744135e6db976486d5
eastonYi <yicheng1994@outlook.com>
2020-08-09 15:02:03 +0800

    fix

--- egs/callhome_hkust/infer.sh
+++ egs/callhome_hkust/infer.sh
@@ -10,12 +10,12 @@ mkdir -p $decode_dir
 
 CUDA_VISIBLE_DEVICES=$gpu \
 python -W ignore::UserWarning $SRC_ROOT/infer.py \
-    --batch_frames 10000 \
+    --batch_frames 50000 \
     --nbest 5 \
-    --use_gpu True \
     --label_type feat_phone_char \
-    $model_type \
-    $expdir/${ep}.pt \
-    data/aishell1_train_chars.txt \
-    data/test.json \
-    $decode_dir/hyp.txt
+    --vocab_char data/vocab_ja.char \
+    --model_type $model_type \
+    --model_pkg $expdir/last.pt \
+    --add_blk False \
+    --json_file data/dev/ja.json \
+    --output $decode_dir/hyp.txt
