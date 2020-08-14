14c0338c1cbfaeb9d83a623331f182fcace406fd
eastonYi <yicheng1994@outlook.com>
2020-08-14 14:16:14 +0800

    add callhome ja ctc recipe

--- egs/callhome_hkust/infer_end2end.sh
+++ egs/callhome_hkust/infer_end2end.sh
@@ -9,7 +9,7 @@ mkdir -p $decode_dir
 
 CUDA_VISIBLE_DEVICES=$gpu \
 python -W ignore::UserWarning $SRC_ROOT/infer.py \
-    --batch_frames 20000 \
+    --batch_frames 10000 \
     --nbest 5 \
     --label_type tokens \
     --vocab_path data/vocab_ja.char \
